cadc0115eb6042f68a86d1990ad0b5bfaf1a72de
solaiman <solaiman6551@gmail.com>
2026-09-09 12:54:35 +0600

    basename cleared

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/solaiman">
+    <HashRouter>
       <ThemeProvider>
         <AuthProvider>
           <App />
